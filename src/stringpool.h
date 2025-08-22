47e9c5fb6712ddc56e1850fc4c6f9849a7b4633a
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2025-08-22 15:50:06 +1000

    Add stringpool

---
+++ src/stringpool.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <xhl/array.h>
+
+typedef struct Stringpool
+{
+    char* buffer; // xarray
+} Stringpool;
+
+static size_t stringpool_push(Stringpool* pool, const char* str, size_t len)
+{
+    size_t offset = xarr_len(pool->buffer);
+    xassert((offset & 15) == 0);
+
+    size_t nextlen = offset + len + 1;       // +1 for '\0' byte
+    nextlen        = (nextlen + 0xf) & ~0xf; // align up by 16 bytes
+    xarr_setlen(pool->buffer, nextlen);
+
+    memcpy(pool->buffer + offset, str, len);
+    pool->buffer[offset + len] = '\0';
+
+    return offset;
+}
