bb242ae8efeb33e3856c07c65df953781ac4c12e
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2026-03-06 11:24:05 +1000

    Add qsort

--- src/sort.h
+++ src/sort.h
@@ -55,3 +55,38 @@ static void sort_char(char* a, int N)
         }
     }
 }
+
+// Note: Windows qsort_s is not the same as C11 qsort_s. F
+//       MacOS qsort_r is not the same as linux qsort_r. F
+//       Fortunately the compare function used by Windows qsort_s is identical to Apples qsort_r, the only difference is
+//       the parameter order to qsort_x.
+// https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/qsort-s?view=msvc-170
+// https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man3/qsort_r.3.html
+// https://en.cppreference.com/w/c/algorithm/qsort
+static inline void xqsort_s(
+    void*              base,
+    unsigned long long num_elements,
+    unsigned long long size_elements,
+    int (*compare_func)(void* ctx, void const* a, void const* b),
+    void* ctx)
+{
+#ifdef _WIN32
+    extern void qsort_s(
+        void*              _Base,
+        unsigned long long _NumOfElements,
+        unsigned long long _SizeOfElements,
+        int (*_CompareFunction)(void*, void const*, void const*),
+        void* _Context);
+    qsort_s(base, num_elements, size_elements, compare_func, ctx);
+#elif defined(__APPLE__)
+    extern void qsort_r(
+        void*              _base,
+        unsigned long long _nel,
+        unsigned long long _width,
+        void*              _thunk,
+        int (*_compar)(void*, const void*, const void*));
+    qsort_r(base, num_elements, size_elements, ctx, compare_func);
+#else
+#error "Platform unsupported"
+#endif
+}
